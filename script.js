b59305b51a7f5d6f590e962332106c4d5132d8cf
stephsoffiati65 <stephanesoffiati65@gmail.com>
2021-09-10 15:53:57 +0200

    Tranformation barre de nav

---
+++ script.js
@@ -0,0 +1,28 @@
+const rectNav = document.querySelector('.rectNav');
+const a = document.querySelectorAll('nav a');
+const button = document.querySelector('.btn-grad ');
+const inscription = document.querySelector('#inscription');
+const insc = document.querySelector('#insc');
+const header = document.querySelector('header');
+let height = header.clientHeight;
+
+
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > height){
+        rectNav.classList.add('scroll');
+        button.classList.add('scroll');
+        inscription.classList.add('scroll');
+        insc.classList.add('scroll');
+        a.forEach(elm=>{ 
+            elm.classList.add('scroll')})
+    } else {
+        rectNav.classList.remove('scroll');
+        button.classList.remove('scroll');
+        inscription.classList.remove('scroll');
+        insc.classList.remove('scroll');
+        a.forEach(elm=>{ 
+            elm.classList.remove('scroll')})
+    }
+});
+
