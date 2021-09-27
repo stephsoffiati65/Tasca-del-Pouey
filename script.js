372451afac50c73e92e34aaf134376344ea453ff
stephsoffiati65 <stephanesoffiati65@gmail.com>
2021-09-27 18:53:10 +0200

    Modif réservation

--- script.js
+++ script.js
@@ -118,4 +118,18 @@ function slidePrecedente () {
 precedent.addEventListener('click', slidePrecedente);
 
 
+// Message d'erreur Inscription
+
+
+// const pwdInput = document.querySelector("#passwordIns");
+// const confirmPwdInput = document.querySelector("#confirmPasswordIns");
+// const formIns = document.querySelector("#formIns");
+
+// document.querySelector("#formIns").addEventListener("submit", e => {
+//     if (pwdInput.value !== confirmPwdInput.value) {
+//         e.preventDefault();
+//         console.log(pwdInput.value);
+//         alert("Le mot de passe et sa confirmation doivent être identiques !");
+//     }
+// });
 
